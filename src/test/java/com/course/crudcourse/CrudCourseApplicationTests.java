67770c813dba6d73b6f39cf8873fbf6767340bb1
Matheus-AMoreira <matheus.moreira2172@gmail.com>
2024-04-14 21:46:37 -0300

    Spring setup

---
+++ src/test/java/com/course/crudcourse/CrudCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.crudcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
