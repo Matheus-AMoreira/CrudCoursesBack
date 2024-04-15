9965e167d87e82e42e1c5692f03db423900d3418
Matheus-AMoreira <matheus.moreira2172@gmail.com>
2024-04-15 09:15:11 -0300

    new file:   README.MD 	modified:   src/main/java/com/course/crudcourse/CrudCourseApplication.java 	new file:   src/main/java/com/course/crudcourse/controller/CourseController.java 	new file:   src/main/java/com/course/crudcourse/model/Course.java 	new file:   src/main/java/com/course/crudcourse/repository/CourseRepository.java 	modified:   src/main/resources/application.properties

---
+++ src/main/java/com/course/crudcourse/repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.course.crudcourse.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.course.crudcourse.model.Course;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long>{
+    
+}
