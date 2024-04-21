ac5de3440e5e23911aecfea922e0652b2f1d4897
Matheus-AMoreira <matheus.moreira2172@gmail.com>
2024-04-20 22:20:41 -0300

    Rota de POST feita

--- src/main/java/com/course/crudcourse/controller/CourseController.java
+++ src/main/java/com/course/crudcourse/controller/CourseController.java
@@ -2,7 +2,11 @@ package com.course.crudcourse.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +25,13 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code= HttpStatus.CREATED)
+    public ResponseEntity<Course> create(@RequestBody Course course){
+        System.out.println(course);
+        return courseRepository.save(course);
+        //return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
     }
 }
